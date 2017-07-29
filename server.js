42689c2a626e30d16ee76bf5dc54002ff34922e8
Sergio Rojas <hello@sergiorojas.me>
2017-07-28 23:05:46 -0700

    Created to respond shorten url.

--- server.js
+++ server.js
@@ -19,8 +19,21 @@ app.post("/validateURL", function(req, res){
 
   if(valid) {
     var stripUrl = url.replace(/^(http|https):\/\/www\./, "");
+
     dns.lookup(stripUrl, function(err, address, family) {
-      console.log("address: %j family: %s", address, family);
+      if(address == undefined) {
+        res.json({
+          "error": "invalid site"
+        });
+      }
+      else {
+          var originalUrl = url;
+          var shortenUrl = generateShortenUrl();
+          res.json({
+            "original_url": url,
+            "shorten_url": shortenUrl
+          });
+      }
     });
   }
   else {
@@ -30,6 +43,39 @@ app.post("/validateURL", function(req, res){
   }
 });
 
+var generateRandomNumber = function() {
+  return Math.floor(Math.random() * 10);
+};
+var generateRandomLetter = function() {
+  var letters = "abcdefghijklmnopqrstuvwxyz";
+  var number = Math.floor(Math.random() * 26);
+
+  return letters[number];
+};
+var generateRandomSymbol = function() {
+  var symbols = "~!@#$%^&*()+?<>";
+  var number = Math.floor(Math.random() * 15);
+
+  return symbols[number];
+};
+var generateRandomHash = function() {
+  var hash = "";
+
+  for(var i = 0; i < 2; i++) {
+    hash += generateRandomNumber();
+    hash += generateRandomLetter();
+    hash += generateRandomSymbol();
+  }
+
+  return hash;
+};
+var generateShortenUrl = function() {
+   var hash = generateRandomHash();
+   var url = "https://tabby-chess.glitch.me/";
+   var shortenUrl = url + hash;
+
+   return shortenUrl;
+};
 app.listen(port, function() {
   console.log("Node.js listening on http://localhost:" + port);
 });
